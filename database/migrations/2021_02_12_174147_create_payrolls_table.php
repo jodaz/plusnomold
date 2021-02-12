8772c26acd5710995030465c9f3e1f2a2e3bac29
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-02-12 13:44:43 -0400

    Add payroll

---
+++ database/migrations/2021_02_12_174147_create_payrolls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayrollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->string('identification');
+            $table->decimal('total_deduction_amount');
+            $table->string('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payrolls');
+    }
+}
