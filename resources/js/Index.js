3aaac91a95cf45b246554b17420d352686655587
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-03-29 16:15:44 -0400

    Move dataProvider

--- resources/js/Index.js
+++ resources/js/Index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Admin, Resource } from 'react-admin';
-import apiClient from 'ra-laravel-client';
 import { createBrowserHistory as createHistory } from 'history';
 import spanishMessages from '@blackbox-vision/ra-language-spanish';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
+import dataProvider from './dataProvider';
 import { Login, Layout } from './layout';
 // Resources
 import dashboard from './dashboard';
@@ -13,14 +13,12 @@ import employees from './employees';
 // Utils
 const customHistory = createHistory();
 import routes from './routes';
-const dataProvider = apiClient('http://localhost:8150/api');
 
 const i18nProvider = polyglotI18nProvider(() => ({
   ...spanishMessages
 }));
 
 export default function App() {
-  console.log(process.env);
   return (
     <Admin
       i18nProvider={i18nProvider}
